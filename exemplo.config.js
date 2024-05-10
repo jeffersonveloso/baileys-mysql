6834c250503686a6f795a5434344d98177c8fd4e
OWEN BRASIL <103150476+owenbrasil@users.noreply.github.com>
2024-05-10 13:34:47 -0300

    Update exemplo.config.js

--- exemplo.config.js
+++ exemplo.config.js
@@ -1,9 +1,9 @@
 // config do db
 // renomeie esse arquivo para config.js
 export default {
-    user: '', // usuarios do db
-    host: '', // host do db
-    database: '', // qual db usar
-    password: '', // senha
-    port: 5432, // porta padrão do Postgres
-}
+    user: 'seu_usuario', // Usuário do banco de dados MySQL
+    host: 'localhost', // Host do banco de dados MySQL (ou o endereço IP do servidor MySQL)
+    database: 'seu_banco_de_dados', // Nome do banco de dados MySQL que você deseja usar
+    password: 'sua_senha', // Senha do banco de dados MySQL
+    port: 3306, // Porta padrão do MySQL
+};
